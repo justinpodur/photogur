0d9a634d8695e95cf3e8785606c289fcf22667ee
git-justinpodur <git-justin@podur.org>
2014-06-17 15:17:25 -0400

    about to delete static picture0.index.html.erb etc.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,26 +1,45 @@
 class PicturesController < ApplicationController
 
-	def picture0
-		@picture = {
-			:title => "Old church on the coast of White Sea",
-			:artist => "Sergey Ershov",
-			:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-		}
-	end
+  def index
+    @pictures = [
+      {
+        :title  => "The old church on the coast of White sea",
+        :artist => "Sergey Ershov",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+      },
+      {
+        :title  => "Sea Power",
+        :artist => "Stephen Scullion",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+      },
+      {
+        :title  => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+      }
+    ]
+  end
 
-	def picture1
-		@picture = {
-			:title => "Sea Power",
-			:artist => "Stephen Scullion",
-			:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-		}
-	end
+  def show
+    @pictures = [
+      {
+        :title  => "The old church on the coast of White sea",
+        :artist => "Sergey Ershov",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+      },
+      {
+        :title  => "Sea Power",
+        :artist => "Stephen Scullion",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+      },
+      {
+        :title  => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+      }
+    ]
+    @picture = @pictures[params[:id].to_i]
+  end
 
-	def picture2
-		@picture =	{
-			:title => "Into the Poppies",
-			:artist => "John Wilhelm",
-			:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-		}
-	end
 end
+
