c0bb936be16bea74284a9a897a6a75b74e7bac7e
git-justinpodur <git-justin@podur.org>
2014-06-17 15:08:14 -0400

    configured routes statically

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,23 +1,26 @@
 class PicturesController < ApplicationController
-	def index
-		@pictures = [
-		{
+
+	def picture0
+		@picture = {
 			:title => "Old church on the coast of White Sea",
 			:artist => "Sergey Ershov",
 			:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-		},
-		{
+		}
+	end
+
+	def picture1
+		@picture = {
 			:title => "Sea Power",
 			:artist => "Stephen Scullion",
 			:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-		},
-		{
+		}
+	end
+
+	def picture2
+		@picture =	{
 			:title => "Into the Poppies",
 			:artist => "John Wilhelm",
 			:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-
 		}
-		]
 	end
-
 end
