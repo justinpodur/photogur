93886931f364d73406d1c2bd6d51dd1ed460f826
git-justinpodur <git-justin@podur.org>
2014-06-17 16:12:46 -0400

    Adding feature to edit pictures

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Photogur::Application.routes.draw do
 
   get 'pictures/:id' => 'pictures#show', as: "picture"
 
-
+  get 'pictures/:id/edit' => 'pictures#edit', as: "edit_picture"
+  patch 'pictures/:id' => "pictures#update"
 
 end
 
