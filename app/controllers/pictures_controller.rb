008cf5244c7bad362ad130f4acf3de867999ea84
git-justinpodur <git-justin@podur.org>
2014-06-17 14:41:16 -0400

    created view

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,23 @@
 class PicturesController < ApplicationController
+	def index
+		@pictures = [
+		{
+			:title => "Old church on the coast of White Sea",
+			:artist => "Sergey Ershov",
+			:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+		},
+		{
+			:title => "Sea Power",
+			:artist => "Stephen Scullion",
+			:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+		},
+		{
+			:title => "Into the Poppies",
+			:artist => "John Wilhelm",
+			:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+
+		}
+		]
+	end
+
 end
